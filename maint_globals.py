0a15826323a4e9a6c71b2f3399509bfff6a3ae0c
Command Prompt, Inc <admins@commandprompt.com>
2016-01-14 19:08:29 -0500

    Create maint_globals.py

---
+++ maint_globals.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+#!/usr/bin/python
+###############################################################################
+### COPYRIGHT NOTICE FOLLOWS.  DO NOT REMOVE
+###############################################################################
+### Copyright (c) 2016 COMMAND PROMPT, INC.
+###
+### Permission to use, copy, modify, and distribute this software and its 
+### documentation for any purpose, without fee, and without a written agreement
+### is hereby granted, provided that the above copyright notice and this paragraph 
+### and the following two paragraphs appear in all copies.
+###
+### IN NO EVENT SHALL COMMAND PROMPT, INC. BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, 
+### INDIRECT SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, 
+### ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+### COMMAND PROMPT, INC. HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+###
+### COMMAND PROMPT, INC. SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT 
+### LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+### PARTICULAR PURPOSE. THE SOFTWARE PROVIDED HEREUNDER IS ON AN "AS IS" BASIS,
+### AND COMMAND PROMPT, INC. HAS NO OBLIGATIONS TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, 
+### ENHANCEMENTS, OR MODIFICATIONS.
+###
+###############################################################################
+
+################################################################################################################
+#
+# Original Author: Michael Vitale, michael@commandprompt.com
+#
+# Description: Globals file, maint_globals.py
+#
+# History:
+# who did it            Date            did what
+# ==========            =========       ==============================
+# Michael Vitale        01/12/2016      Defined constant globals here
+################################################################################################################
+
+#globals
+SUCCESS   = 0
+ERROR     = -1
+ERROR2    = -2
+ERROR3    = -3
+WARNING   = -4
+DEFERRED  = 1
+NOTICE    = 2
+
+DESCRIPTION="This python utility program performs PostgreSQL maintenance tasks."
+VERSION    = 1.0
+PROGNAME   = "pg_maint"
+
+
