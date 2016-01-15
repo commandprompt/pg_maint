c88a9aac6a436d0d66776b0b6655bb6592a74730
Command Prompt, Inc <admins@commandprompt.com>
2016-01-14 19:35:38 -0500

    Update pg_maint.py

--- pg_maint.py
+++ pg_maint.py
@@ -32,8 +32,13 @@
 # -w <max rows> -a [action: ANALYZE, VACUUM_ANALYZE, VACUUM_FREEZE, REPORT] -r [dry run] -s [smart mode] -v [verbose output]
 #
 # Examples:
+#
 # -- vacuum analyze for all user tables in the database but only if load is less than 20% and rows < 1 mil
 # ./pg_maint.py -h localhost -d test -p 5433 -u postgres -a vacuum_analyze -l 20 -w 1000000
+#
+# -- same thing as previous one, but do a dry run.  This is useful to see wht commands will be executed, or is also 
+#    useful for generating DDL so you can run it manually
+# ./pg_maint.py -h localhost -d test -p 5433 -u postgres -a vacuum_analyze -l 20 -w 1000000 -r
 # 
 # -- smart analyze for all user tables in specific schema, but only if load is less than 40% and rows < 1 mil
 # ./pg_maint.py -h localhost -d test -n public -p 5433 -s -u postgres -a analyze -l 40 -w 1000000 
@@ -58,7 +63,7 @@
 #
 # -s (smart mode) dictates a filter algorithm to determine what tables will qualify for the maintenance commands.
 # For analyze and vacuum analyze:
-#     1. Refresh tables with no recent analyze or autovacuum_analyze in the last 30 days.
+#     1. Refresh tables with no recent analyze or autovacuum_analyze in the last 60 days.
 #     2. Refresh tables where pg_stat_user_tables.n_live_tup is less than half of pg_class.reltuples
 # For vacuum freeze:
 #     1. Refresh tables where current high XID age divided by autovacuum_freeze_max_age > 70%.
@@ -76,13 +81,13 @@
 #
 # Report logic:
 #  1. Get database conflicts, deadlocks, and temp_files.
-#  1. Unused indexes are identified where there are less than 20 index scans and thee size of the table is > 100 MB.
-#  2. Bloated tables/indexes are identified where at least 20% of the table/index is bloated or the wasted bytes is > 1GB.
-#  3. See if archiving is getting behind by more than 1000 WAL files.
-#  4. Contrast PG memory configuration to recommended ones
-#  5. Identify orphaned large objects.
-#  6. List tables getting close to transaction wraparound (more than halfway to max freeze threshold).
-#  7. list tables that have not been analyzed or vacuumed in the last 60 days or whose size has grown significantly. 
+#  2. Unused indexes are identified where there are less than 20 index scans and thee size of the table is > 100 MB.
+#  3. Bloated tables/indexes are identified where at least 20% of the table/index is bloated or the wasted bytes is > 1GB.
+#  4. See if archiving is getting behind by more than 1000 WAL files.
+#  5. Contrast PG memory configuration to recommended ones
+#  6. Identify orphaned large objects.
+#  7. List tables getting close to transaction wraparound (more than halfway to max freeze threshold).
+#  8. list tables that have not been analyzed or vacuumed in the last 60 days or whose size has grown significantly. 
 #
 # TODOs:
 #    1. Handle pg_stat_activity to be compatible across pg versions. 
